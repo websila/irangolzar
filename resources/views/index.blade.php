dd891fedda38ecc73275e912d3bed0b22cbe2da1
alirezamohamadi <alirezamkco@gmail.com>
2020-01-27 23:50:28 +0330

    updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ایرازار</title>
+    <title>سلام</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
     <!-- Latest compiled and minified CSS -->

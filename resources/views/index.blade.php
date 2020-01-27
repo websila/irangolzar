650f2065c3e5f0a2e17d4d648d81f78ff2d5ac56
mostafa <mostafa.hajirezai@ut.ac.ir>
2020-01-27 23:41:31 +0330

    index changed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ایران گلزار</title>
+    <title>ایرازار</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
     <!-- Latest compiled and minified CSS -->

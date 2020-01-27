9aec985b0783e52a3867f2afbbc9a8dbfd2978a9
mostafa <mostafa.hajirezai@ut.ac.ir>
2020-01-28 00:00:32 +0330

    front-end updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>سلام</title>
+    <title>ایران گلزار</title>
     <link rel="stylesheet" href="css/style.css">
     <script src="js/script.js"></script>
     <!-- Latest compiled and minified CSS -->
@@ -53,35 +53,37 @@
     <div class="container">
         <div class="row productList">
             @foreach($ads as $ad)
-            <div class="col-12 adCont mt-2">
-                <div class="row position-relative">
-                    <div class="shareTime">
-                        <i class="far fa-clock"></i>
-                        {{$ad->date}}
-                    </div>
-                    <div class="col-4 adRight">
-                        <img src="{{$ad->thumbnail}}" alt="">
-                    </div>
-                    <div class="col-8 pl-0 adLeft position-relative">
-                        <div class="row adTitle">
-                            <div class="col-12">
-                                {{$ad->title}}
+                <a href="/ad/{{$ad->id}}">
+                    <div class="col-12 adCont mt-2">
+                        <div class="row position-relative">
+                            <div class="shareTime">
+                                <i class="far fa-clock"></i>
+                                {{$ad->date}}
                             </div>
-                        </div>
-                        <div class="row adPrice mt-1">
-                            <div class="col-12">
-                                {{$ad->price}}
-                                تومان
+                            <div class="col-4 adRight">
+                                <img src="{{$ad->thumbnail}}" alt="">
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
+                            <div class="col-8 pl-0 adLeft position-relative">
+                                <div class="row adTitle">
+                                    <div class="col-12">
+                                        {{$ad->title}}
+                                    </div>
+                                </div>
+                                <div class="row adPrice mt-1">
+                                    <div class="col-12">
+                                        {{$ad->price}}
+                                        تومان
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12">
 
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </a>
             @endforeach
         </div>
     </div>
